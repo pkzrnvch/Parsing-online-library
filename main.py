d15e0b5e82d3b4cece5f48df2db8085d0ce59b9f
pkzrnvch <pkazarnovich@gmail.com>
2022-03-13 05:30:50 +0500

    Parse book title and author

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
+
 
 def check_for_redirect(response):
     if response.history:
@@ -21,5 +23,24 @@ def download_book(id):
     except requests.HTTPError:
         print('Книга не найдена')
 
+
+def parse_book_title_and_author(id):
+    url = f'http://tululu.org/b{id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    print(response.history)
+    try:
+        check_for_redirect(response)
+        soup = BeautifulSoup(response.text, 'lxml')
+        h1_tag = soup.find('h1')
+        h1_text = h1_tag.get_text()
+        title, author = h1_text.split('::')
+        title = title.strip()
+        author = author.strip()
+        print(title, author)
+    except requests.HTTPError:
+        pass
+
+
 for i in range(10):
-    download_book(i+1)
+    parse_book_title_and_author(i+1)
