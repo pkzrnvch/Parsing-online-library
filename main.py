1512c169dad35440bec2d1fe05037474e3748460
pkzrnvch <pkazarnovich@gmail.com>
2022-03-14 00:20:05 +0500

    Download books with titles

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
-
+from pathvalidate import sanitize_filename
+import os
 
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
 
 
-def download_book(id):
-    url = 'http://tululu.org/txt.php'
-    payload = {'id': id}
-    response = requests.get(url, params=payload)
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    response = requests.get(url)
     response.raise_for_status()
-    print(response.history)
-    try:
-        check_for_redirect(response)
-        file_path = Path(f'books/id{id}.txt')
-        file_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(file_path, 'wt') as file:
-            file.write(response.text)
-    except requests.HTTPError:
-        print('Книга не найдена')
+    check_for_redirect(response)
+    filename = sanitize_filename(filename) + '.txt'
+    filepath = os.path.join(folder, filename)
+    os.makedirs(folder, exist_ok=True)
+    with open(filepath, 'wt') as file:
+        file.write(response.text)
+    return filepath
 
 
 def parse_book_title_and_author(id):
     url = f'http://tululu.org/b{id}/'
     response = requests.get(url)
     response.raise_for_status()
-    print(response.history)
+    check_for_redirect(response)
+    soup = BeautifulSoup(response.text, 'lxml')
+    h1_tag = soup.find('h1')
+    h1_text = h1_tag.get_text()
+    title, author = h1_text.split('::')
+    title = title.strip()
+    author = author.strip()
+    return title, author
+
+
+def download_tululu_book(id):
+    url = f'http://tululu.org/txt.php?id={id}'
     try:
-        check_for_redirect(response)
-        soup = BeautifulSoup(response.text, 'lxml')
-        h1_tag = soup.find('h1')
-        h1_text = h1_tag.get_text()
-        title, author = h1_text.split('::')
-        title = title.strip()
-        author = author.strip()
-        print(title, author)
+        title, author = parse_book_title_and_author(id)
+        print(download_txt(url, title))
     except requests.HTTPError:
-        pass
+        print('Книга не найдена')
 
 
 for i in range(10):
-    parse_book_title_and_author(i+1)
+    download_tululu_book(i+1)
