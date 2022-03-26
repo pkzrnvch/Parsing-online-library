148255f5d65f1af4765a4b2226ae10b48c10c5a2
pkzrnvch <pkazarnovich@gmail.com>
2022-03-27 00:00:06 +0500

    Corrections and minor improvements

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -15,18 +15,16 @@ def get_end_page_to_parse(url):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    end_page = soup.select('.npage')[-1]
+    end_page = soup.select('.npage')[-1].text
+    print(end_page)
     return end_page
 
 
-def parse_category_for_book_urls(start_page, end_page):
+def parse_category_for_book_urls(start_page, end_page, category_url):
     book_urls = []
-    base_url = 'https://tululu.org/l55/'
-    if not end_page:
-        end_page = get_end_page_to_parse(base_url)
     for page_to_parse in range(start_page, end_page+1):
         with suppress(requests.HTTPError):
-            url = f'{base_url}{page_to_parse}/'
+            url = f'{category_url}{page_to_parse}/'
             response = requests.get(url)
             response.raise_for_status()
             soup = BeautifulSoup(response.text, 'lxml')
@@ -59,51 +57,57 @@ def download_tululu_book(url, book_folder, img_folder, skip_txt, skip_img):
 
 
 def main():
+    category_url = 'https://tululu.org/l55/'
     parser = argparse.ArgumentParser(
-        description="This script downloads books, book covers and parses book descriptions."
+        description="This script downloads books, book covers "
+                    "and parses book descriptions."
     )
     parser.add_argument(
         "--start_page",
         type=int,
         help="first category page to get books from",
-        default=1
+        default=1,
     )
     parser.add_argument(
         "--end_page",
         type=int,
         help="last category page to get books from",
-        default=4
+        default=get_end_page_to_parse(category_url),
     )
     parser.add_argument(
         "--dest_folder",
         help="destination folder",
-        default='.'
+        default='.',
     )
     parser.add_argument(
         "--skip_imgs",
         help="skip downloading book covers",
-        action="store_true"
+        action="store_true",
     )
     parser.add_argument(
         "--skip_txt",
         help="skip downloading .txt files",
-        action="store_true"
+        action="store_true",
     )
     parser.add_argument(
         "--json_path",
         help="path to the json file with downloaded books description",
-        default='./downloaded_books.json'
+        default='./downloaded_books.json',
     )
     args = parser.parse_args()
     book_folder = sanitize_filepath(f'{args.dest_folder}/books')
     img_folder = sanitize_filepath(f'{args.dest_folder}/images')
     json_path = sanitize_filepath(args.json_path)
-    print(json_path)
     if json_path != 'downloaded_books.json':
         os.makedirs(os.path.dirname(json_path), exist_ok=True)
     os.makedirs(book_folder, exist_ok=True)
     os.makedirs(img_folder, exist_ok=True)
-    book_urls = parse_category_for_book_urls(args.start_page, args.end_page)
+    print(type(args.end_page))
+    book_urls = parse_category_for_book_urls(
+        args.start_page,
+        args.end_page,
+        category_url,
+    )
     downloaded_books = []
     for url in book_urls:
         with suppress(requests.HTTPError):
